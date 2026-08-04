dc553018433f2efc6e874198e9febaa4fff0579a
anonymous Del Barco <61146153+maridasi47500@users.noreply.github.com>
2026-08-04 22:13:58 +0200

    wow dljfghsfj

---
+++ demofile.sh
@@ -0,0 +1,5 @@
+
+mkdir templates 
+python3 scaffold.py user username phone email country_id:references
+python3 scaffold.py iphoneidentity user_id:references pic:file
+python3 scaffold.py country name
